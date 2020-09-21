f90a455f012e0ab8a452c38a6bcfb36a1ba55496
Joao Pedro <joao.ricardo88@gmail.com>
2020-09-21 17:43:17 +0100

    alert test message

--- colorGame.js
+++ colorGame.js
@@ -8,7 +8,17 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor;
 
 for (var i=0; i< squares.length; i++){
+    //add initial colors to squares
     squares[i].style.backgroundColor = colors[i]
+
+    //add click listiners to squares
+    squares[i].addEventListener("click", function(){
+     //grab color of clicked square
+     alert(this.style.backgroundColor);    
+    });
 }
